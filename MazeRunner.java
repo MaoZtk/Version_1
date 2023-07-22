6786edcfe38fc8b13419db3f8603d2e57707790e
MaoZtk <140282592+MaoZtk@users.noreply.github.com>
2023-07-22 16:07:11 -0600

    Add files via upload

---
+++ MazeRunner.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MazeRunner here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MazeRunner extends Movers
+{
+    /**
+     * Act - do whatever the MazeRunner wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    //Create our Constructor Method
+    public MazeRunner()
+    {
+        getImage().scale(getImage().getWidth()/2, getImage().getHeight()/2);
+    }
+    public void act()
+    {
+      slideAround();
+      collectBoost();
+      maxSpeed();
+    }
+}
