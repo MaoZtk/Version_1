07486d5fa87eeb9c29a4432c557cb8e28cb5f35f
MaoZtk <oomarielos@gmail.com>
2023-07-30 12:58:55 -0600

    Música Agregada
    Se le agregó música al juego al empezar desde la pantalla de inicio (MyWorld)

--- MazeWorld1.java
+++ MazeWorld1.java
@@ -13,6 +13,7 @@ public class MazeWorld1 extends World
      * Constructor for objects of class MazeWorld1.
      * 
      */
+    
     public MazeWorld1()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
--- MyWorld.java
+++ MyWorld.java
@@ -13,16 +13,30 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    private Musica musicaFondo;
+    private boolean juegoIniciado;
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 600, 1); 
+        musicaFondo = new Musica("Dogs and Cats.mp3");
+        juegoIniciado = false;
     }
     public void act()
     {
-        if(Greenfoot.isKeyDown("space"))
+        if (!juegoIniciado) 
         {
-            Greenfoot.setWorld(new MazeWorld1()); 
+            // Reproducir música solo si el juego aún no ha comenzado
+            musicaFondo.reproducir();
+            juegoIniciado = true;
         }
+        if (Greenfoot.isKeyDown("space")) 
+        {
+            Greenfoot.setWorld(new MazeWorld1());
+        }
+    }
+    public void detenerMusica() 
+    {
+        musicaFondo.detener();
     }
 }
